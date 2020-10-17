f46baad2fe73b84fd41b1358f809979125b52156
mariiama <mariiamalisheva@gmail.com>
2020-10-17 21:13:35 +0300

    Part 1 test

--- src/test/java/com/example/tesktask/SearchItimTestsForMarket.java
+++ src/test/java/com/example/tesktask/SearchItimTestsForMarket.java
@@ -6,11 +6,11 @@ public class SearchItimTestsForMarket extends TestBase{
 
 
     @Test
-    public void testItem()
-    {
+    public void testItem() throws InterruptedException {
         goToMarket();
         switchToNextTab();
         selectComputersDepartment();
+        Thread.sleep(2000);
         filterItem(new Item("planshety", "20000", "35000", "Apple"));
         String itemName = wd.findElement (By.xpath("//*[@data-autotest-id='product-snippet'][2]//h3")).getText();
         System.out.println(itemName);
